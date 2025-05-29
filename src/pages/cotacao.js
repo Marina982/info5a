e6c2f925b23a82f1dc6918e7580bef98a477f227
Marina982 <marina.goes@estudante.ifms.edu.br>
2025-05-29 15:43:50 -0300

    save

--- src/pages/cotacao.js
+++ src/pages/cotacao.js
@@ -3,7 +3,7 @@ import { fetcher } from '../lib/fetcher';
 
 export default function Home() {
   const { data, error, isLoading } = useSWR(
-    'https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL?token=927c456f9a4bec44887e5cc0e2d154c8f843f33855ec2ec0d15db596ee7d19cd',
+    'https://economia.awesomeapi.com.br/json/daily/USD-BRL/365?start_date=20240101&end_date=20241231',
     fetcher,
     { refreshInterval: 5000 } // Atualiza a cada 60s //1000 == 1 segundo refresh page
   );
